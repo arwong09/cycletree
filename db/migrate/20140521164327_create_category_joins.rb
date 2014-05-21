7dafd785b8c15afd6f12da10a3843f09e43d8de6
Andrew Wong <arwong09@gmail.com>
2014-05-21 10:42:12 -0700

    migrate category joins

--- db/migrate/20140521164327_create_category_joins.rb
+++ db/migrate/20140521164327_create_category_joins.rb
@@ -1,7 +1,8 @@
 class CreateCategoryJoins < ActiveRecord::Migration
   def change
     create_table :category_joins do |t|
-
+      t.integer :item_id, null: false
+      t.integer :category_id, null: false
       t.timestamps
     end
   end
