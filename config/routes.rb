61afc7f816e51bb697b5fd3e2b868b9f543d0ddf
Andrew Wong <arwong09@gmail.com>
2014-05-21 00:43:24 -0700

    create new session view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Cycletree::Application.routes.draw do
+  resources :users
+  resource :session
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
