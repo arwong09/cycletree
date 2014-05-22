a47a7e67da26c1e809d9edabad3f006f363ec737
Andrew Wong <arwong09@gmail.com>
2014-05-22 14:25:48 -0700

    form styling, just about to push to heroku

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ActiveRecord::Base
   has_many :category_joins, class_name: :CategoryJoin, foreign_key: :category_id
   has_many :items, through: :category_joins, source: :item
-  has_attached_file :image, styles: { thumb: "100x100#", small: "200x200>" }
+  has_attached_file :image, styles: { thumb: "250x250#", small: "400x400>" }
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
