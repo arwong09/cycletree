9ea75cc3bf7b03d2854068893959dd16cd64450e
Andrew Wong <arwong09@gmail.com>
2014-05-23 11:25:18 -0700

    implement variable image sizing on category show page

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ActiveRecord::Base
   has_many :category_joins, class_name: :CategoryJoin, foreign_key: :item_id
   has_many :categories, through: :category_joins, source: :category
   has_attached_file :image, styles: { show: "570x", thumb: "" }, convert_options: { 
-    thumb: "-resize 300 -gravity Center -crop 200x500+0+0" },
+    thumb: "-resize 300x1500 -gravity Center -shave 45x0" },
     default_style: :thumb
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
