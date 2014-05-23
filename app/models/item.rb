cc3081c7b74031920e03dfbaff7905889214204a
Andrew Wong <arwong09@gmail.com>
2014-05-22 17:12:45 -0700

    image resizing

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,8 @@ class Item < ActiveRecord::Base
   belongs_to :owner, class_name: :User, foreign_key: :owner_id
   has_many :category_joins, class_name: :CategoryJoin, foreign_key: :item_id
   has_many :categories, through: :category_joins, source: :category
-  has_attached_file :image, styles: { thumb: "220x660#", small: "330x440>" }
+  has_attached_file :image, styles: { thumb: "" }, convert_options: { 
+    thumb: "-resize 300x +repage -gravity Center -crop 200x500#" },
+    default_style: :thumb
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
