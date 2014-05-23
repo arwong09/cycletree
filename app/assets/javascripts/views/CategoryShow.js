35dadb5514ada49278675f4510894fe6ebb03346
Andrew Wong <arwong09@gmail.com>
2014-05-23 12:03:48 -0700

    Variable heights working, 4 columns

--- app/assets/javascripts/views/CategoryShow.js
+++ app/assets/javascripts/views/CategoryShow.js
@@ -2,7 +2,13 @@ Cycletree.CategoriesShow = Backbone.View.extend({
   template: JST['categories/show'],
   
   render: function() {
-    var renderedContent = this.template({items: this.collection});
+    var colSize = Math.floor(this.collection.length / 4);
+    var items1 = new Cycletree.Items(this.collection.slice(0, colSize), {category_id: this.collection.category_id});
+    var items2 = new Cycletree.Items(this.collection.slice(colSize, colSize * 2), {category_id: this.collection.category_id});
+    var items3 = new Cycletree.Items(this.collection.slice(colSize * 2, colSize * 3), {category_id: this.collection.category_id});
+    var items4 = new Cycletree.Items(this.collection.slice(colSize * 3), {category_id: this.collection.category_id});
+
+    var renderedContent = this.template({items1: items1, items2: items2, items3: items3, items4 : items4});
     this.$el.html(renderedContent);
     return this;
   },
