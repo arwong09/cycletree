a5de3a31a7373ebf2f00752921d2ca70ba28764c
Andrew Wong <arwong09@gmail.com>
2014-05-23 13:23:27 -0700

    begin refactoring parseColumns function:

--- app/assets/javascripts/views/CategoryShow.js
+++ app/assets/javascripts/views/CategoryShow.js
@@ -2,12 +2,8 @@ Cycletree.CategoriesShow = Backbone.View.extend({
   template: JST['categories/show'],
   
   render: function() {
-    var colSize = Math.floor(this.collection.length / 4);
-    var items1 = new Cycletree.Items(this.collection.slice(0, colSize), {category_id: this.collection.category_id});
-    var items2 = new Cycletree.Items(this.collection.slice(colSize, colSize * 2), {category_id: this.collection.category_id});
-    var items3 = new Cycletree.Items(this.collection.slice(colSize * 2, colSize * 3), {category_id: this.collection.category_id});
-    var items4 = new Cycletree.Items(this.collection.slice(colSize * 3), {category_id: this.collection.category_id});
-
+    var columns = this.collection.parseColumns(4);
+    
     var renderedContent = this.template({items1: items1, items2: items2, items3: items3, items4 : items4});
     this.$el.html(renderedContent);
     return this;
@@ -18,6 +14,8 @@ Cycletree.CategoriesShow = Backbone.View.extend({
     "click #options-all": "render",
     "click #options-used": "filterUsed"
   },
+  
+  parseColumns: function()
   
   filterNew: function() {
     var filteredArr = this.collection.where({condition: "New"});
