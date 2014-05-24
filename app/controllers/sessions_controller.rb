02eecce68022315a6c36e35b5727af54bcac00bc
Andrew Wong <arwong09@gmail.com>
2014-05-23 22:46:05 -0700

    adding to profile

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
   
     if @user
       login!(@user)
-      redirect_to user_url(@user)
+      redirect_to edit_user_url(@user)
     else
       flash.now[:errors] = ["Wrong username or password"]
       render :new
