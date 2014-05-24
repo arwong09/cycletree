2a7bfc7092aef600020543d1d78f0b92cd4f920c
Andrew Wong <arwong09@gmail.com>
2014-05-23 17:55:38 -0700

    profile view now renders 3 other listings

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ActiveRecord::Base
   belongs_to :owner, class_name: :User, foreign_key: :owner_id
   has_many :category_joins, class_name: :CategoryJoin, foreign_key: :item_id
   has_many :categories, through: :category_joins, source: :category
-  has_attached_file :image, styles: { show: "570x", thumb: "" }, convert_options: { 
+  has_attached_file :image, styles: { profile_listing: "230x153", show: "570x", thumb: "" }, convert_options: { 
     thumb: "-resize 280x1500 -gravity Center -shave 58x0" },
     default_style: :thumb
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
