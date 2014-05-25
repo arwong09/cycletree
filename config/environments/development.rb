1da85640847c232364534c6ca8d9141e4627c04a
Andrew Wong <arwong09@gmail.com>
2014-05-25 16:12:08 -0700

    finish installing figaro and aws keys

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,15 @@
 Cycletree::Application.configure do
+  config.paperclip_defaults = {                                   
+    :storage => :s3,                                              
+    :s3_protocol => 'http',                                       
+    :url =>':s3_domain_url',                                      
+    :path => '/:class/:attachment/:id_partition/:style/:filename',
+    :s3_credentials => {                                          
+      :bucket => ENV['AWS_BUCKET'], #these values safely stored in application.yml thanks to figaro!                
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],                 
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']          
+    }                                                             
+  }     
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
