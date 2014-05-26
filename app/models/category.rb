53f18de814eddeb19509433ebb1e738056ca2f02
Andrew Wong <arwong09@gmail.com>
2014-05-25 22:05:30 -0700

    significant progress on index page

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ActiveRecord::Base
   has_many :category_joins, class_name: :CategoryJoin, foreign_key: :category_id
   has_many :items, through: :category_joins, source: :item
-  has_attached_file :image, styles: { index_large: "468x382#", index_small: "222x186#", thumb: "180x500#", small: "140x140>" }
+  has_attached_file :image, styles: { index_large: "468x382#", index_small: "216x186#", thumb: "180x500#", small: "140x140>" }
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
