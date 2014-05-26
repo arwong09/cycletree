c5f1c02cbe3e67b9b6c8bd37ccfd10f897812796
Andrew Wong <arwong09@gmail.com>
2014-05-25 23:16:45 -0700

    create seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+categories = Category.create([{ title: 'Road', image: File.new('images/road.jpg', "r") }, 
+  { title: 'Cruiser', image: File.new('images/cruiser.jpg', "r")},
+  { title: 'Fixed Gear', image: File.new('images/fixie2.png', "r")},
+  { title: 'Mountain', image: File.new('images/mountain.jpg', "r")},
+  { title: 'Cruiser', image: File.new('images/cruiser.jpg', "r")},
+  { title: 'Vintage', image: File.new('images/vintage.jpg', "r")},
+])
