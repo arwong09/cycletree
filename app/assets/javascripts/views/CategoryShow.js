7917ad99d5607563a74f92040f51c3f98b36a71d
Andrew Wong <arwong09@gmail.com>
2014-05-29 13:09:32 -0700

    first phase of mulit-filter search working

--- app/assets/javascripts/views/CategoryShow.js
+++ app/assets/javascripts/views/CategoryShow.js
@@ -1,6 +1,41 @@
 Cycletree.CategoriesShow = Backbone.View.extend({
   template: JST['categories/show'],
   
+  initialize: function() {
+    this.listenTo(this.collection, 'sync', this.render);
+    // this.filteredCollection = this.collection;
+    this.priceFiltered = this.collection;
+    this.searchFiltered = this.collection;
+    this.condFiltered = this.collection;
+  },
+  
+  events: {
+    "click #options-new": "condFilter",
+    "click #options-all": "condFilter",
+    "click #options-used": "condFilter",
+    "click .options-price": "priceFilter",
+    "click #options-price-any": "priceFilter",
+    "keyup #search-filter": "searchFilter"
+  },
+  
+  // multiFilter: function(event) {
+  //   this.condFilter(event);
+  //   // this.priceFilter(event);
+  //   multiFiltered = this.condFiltered;
+  // 
+  //   // var priceAndCondFiltered = cond
+  //   // this.priceFilter(event);
+  //   // this.searchFilter(event);
+  //   debugger
+  //   
+  //   if (this.priceFiltered.length > 0) {
+  //     var multiFiltered = this.condFiltered.filter(function(item) {
+  //       return this.priceFiltered.indexOf(item) != -1;
+  //     })
+  //   }
+  //   this.filteredRender(multiFiltered);
+  // },
+  
   render: function() {
     var columns = this.collection.parseColumns(4);
     var items1 = columns[0];
@@ -27,10 +62,11 @@ Cycletree.CategoriesShow = Backbone.View.extend({
     return this;
   },
   
-  filteredRender: function(filteredCollection) {
-    if (filteredCollection.length === 0) {
-      this.$el.html("nothing found");
-    } else {
+  filteredRender: function() {
+    var filteredArr = this.condFiltered.filter(function(item){
+      return this.priceFiltered.indexOf(item) != -1;
+    }.bind(this))
+    var filteredCollection = new Cycletree.Items(filteredArr, {category_id: this.collection.category_id});
       var columns = filteredCollection.parseColumns(4);
       var items1 = columns[0];
       var items2 = columns[1];
@@ -39,10 +75,24 @@ Cycletree.CategoriesShow = Backbone.View.extend({
     
       var renderedContent = this.template({items1: items1, items2: items2, items3: items3, items4 : items4});
       this.$el.html(renderedContent);
-    }
     return this;
-
   },
+  
+  // filteredRender: function(filteredCollection) {
+ //    if (filteredCollection.length === 0) {
+ //      this.$el.html("nothing found");
+ //    } else {
+ //      var columns = filteredCollection.parseColumns(4);
+ //      var items1 = columns[0];
+ //      var items2 = columns[1];
+ //      var items3 = columns[2];
+ //      var items4 = columns[3];
+ //    
+ //      var renderedContent = this.template({items1: items1, items2: items2, items3: items3, items4 : items4});
+ //      this.$el.html(renderedContent);
+ //    }
+ //    return this;
+ //  },
   
   searchRender: function(filteredCollection, prevQuery) {
     var columns = filteredCollection.parseColumns(4);
@@ -57,25 +107,17 @@ Cycletree.CategoriesShow = Backbone.View.extend({
     return this;
   },
   
-  events: {
-    "click #options-new": "filter",
-    "click #options-all": "render",
-    "click #options-used": "filter",
-    "click .options-price": "priceFilter",
-    "click #options-price-any": "render",
-    "keyup #search-filter": "searchFilter"
-  },
-  
-  filter: function(event) {
+  condFilter: function(event) {
     var condition = $(event.target).data('condition');
     var filteredArr = this.collection.where({condition: condition});
     var filteredCollection = new Cycletree.Items(filteredArr, {category_id: this.collection.category_id});
-    this.filteredRender(filteredCollection);
+    // this.filteredRender(filteredCollection);
+    this.condFiltered = filteredCollection;
     
     this.$('.btn-options').removeClass('active');
     var id = $(event.target).attr('id')
     this.$('#' + id).addClass('active');
-  
+    this.filteredRender();
     return this;
   },
     
@@ -89,7 +131,10 @@ Cycletree.CategoriesShow = Backbone.View.extend({
     })
     
     var filteredCollection = new Cycletree.Items(filteredArr, {category_id: this.collection.category_id});
-    this.filteredRender(filteredCollection);
+    // this.filteredCollection = filteredCollection;
+    this.priceFiltered = filteredCollection;
+    this.filteredRender();
+    // this.filteredRender(filteredCollection);
   },
   
   searchFilter: function(event, keyword) {
@@ -97,20 +142,16 @@ Cycletree.CategoriesShow = Backbone.View.extend({
       var inputString = keyword;
     } else { var inputString = $(event.target).val(); }
     
-    // if (inputString) {
-      searchString = inputString.replace(/bikes/g, "").replace(/bike/g, "");
-    // }
-    
+    searchString = inputString.replace(/bikes/g, "").replace(/bike/g, "").replace(/bicycle/g, "").replace(/bicycles/g, "");
 
-    var filteredArr = this.collection.filter(function(item) {
+    var filteredArr = this.filteredCollection.filter(function(item) {
       return item.get('title').toLowerCase().indexOf(searchString) != -1 || item.get('owner').toLowerCase().indexOf(searchString) != -1 || item.get('category').toLowerCase().indexOf(searchString) != -1;
     })
     
     var filteredCollection = new Cycletree.Items(filteredArr, {category_id: this.collection.category_id});
-    this.searchRender(filteredCollection, inputString);
+    // this.searchRender(filteredCollection, inputString);
+    this.filteredCollection = filteredCollection;
   },
   
-  initialize: function() {
-    this.listenTo(this.collection, 'sync', this.render);
-  }
+ 
 }); 
